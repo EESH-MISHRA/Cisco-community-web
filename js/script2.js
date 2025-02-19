46dae96ff536cb6948b2369545461513e8ae1157
Aditya Tiwari <72137687+AdityaTiwari64@users.noreply.github.com>
2025-02-19 18:58:02 +0530

    eventspage

--- js/script2.js
+++ js/script2.js
@@ -1,7 +1,9 @@
 const timeline_wrapper = document.querySelector('.timeline-wrapper'),
 timelines = document.querySelectorAll('.timeline li .data')
 for(const time of timelines){
-    time.onclick = ()=>time.classList.toggle('show')
+    time.onclick = ()=>{
+        time.classList.toggle('show')   
+    }
 }
 timeline_wrapper.addEventListener('mousemove' ,(event)=>{
     const timeline = document.querySelector('.timeline')
